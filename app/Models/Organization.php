355eeaff278093e30e934c4823db01616544092f
rainistiirik23 <rainis.tiirik9@gmail.com>
2025-10-09 21:34:18 +0300

    Belongs to many relationship for parent and daughter organization retrival

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -3,5 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class Organization extends Model {}
+class Organization extends Model
+{
+    public function parents(): BelongsToMany
+    {
+        return $this->BelongsToMany(Organization::class, 'organization_relationships', 'daughter_id', 'parent_id');
+    }
+    public function daughters(): BelongsToMany
+    {
+        return $this->BelongsToMany(Organization::class, 'oganization_relationships', 'parent_id', 'daughter_id');
+    }
+}
